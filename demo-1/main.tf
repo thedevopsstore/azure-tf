4f5c0e82405da38513f2ab5ddc427d3c19d00e5c
Kiran jordan <kiranjordan@Scevas-Air.attlocal.net>
2022-08-29 21:59:23 -0400

    initial

---
+++ demo-1/main.tf
@@ -0,0 +1,21 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+}
+
+# Create resource group
+
+resource "azurerm_resource_group" "rg" { 
+name = "lwplabsrg" 
+location = "East US 2" 
+} 
